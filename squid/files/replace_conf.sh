63beb91999c29373130cb2a36dd64b9ed55afe2a
dansta <danne@kvantmekanik.se>
2017-12-13 11:32:40 +0100

    Generic configuration, growing towards boilerplate-esque system

--- squid/files/replace_conf.sh
+++ squid/files/replace_conf.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
-# set up env
-set -e
 
-CONF=$1
+# usage: ./replace_conf.sh /etc/app/app.conf APP
+# first parameter is path to config file
+# second parameter is environment variabel to use for replacement in the config file
+
+# set up env
+set -x # print loops
+set -e # exit immediately on bg job errors
+set -u # exit on unset vars
+set -v # debugging
+if [ -z "$3" ]; then
+  echo "Only two arguments supported"
+  exit 1
+fi
+
+CONF="$1"
 CONFDATA="$(cat $CONF)"
 KEYAWK="/usr/bin/awk -f /usr/local/bin/key.awk"
 VALUEAWK="/usr/bin/awk -f /usr/local/bin/value.awk"
 
 
-# get all the squidvars
-SQUIDENV="$(env | grep SQUID)"
+# get all the vars
+ENVCONF="$(env | grep $2)"
 
 # for each squidvar, replace them in the file
-for i in $SQUIDENV; do
+for i in $ENVCONF; do
   CONFDATA="$(echo -e "$CONFDATA" | sed s/"$(echo $i | $KEYAWK)"/"$(echo $i | $VALUEAWK)"/g )"
 done
 
