120aa415eb6955c073ad005519456e1cd9be3feb
dansta <danne@kvantmekanik.se>
2017-12-16 08:01:58 +0100

    Trying this

--- squid/files/replace_conf.sh
+++ squid/files/replace_conf.sh
@@ -14,10 +14,10 @@ INVALID="0"
 # second parameter is environment variabel to use for replacement in the config file
 
 # set up env
-#set -x # print loops
+set -x # print loops
 set -e # exit immediately on bg job errors
 #set -u # exit on unset vars, like empty configs, no env var etc
-set -v # debugging
+#set -v # debugging
 
 # read the config as an array
 while read -r line; do 
