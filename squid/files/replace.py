fa68e2304df374500a1cf03ff6c0f87cfdafadbb
Daniel Ståhl <danne@kvantmekanik.se>
2017-12-20 14:21:27 +0100

    Replace template config placeholders based on key prefix
    This one works locally. No idea if it works in docker hub.

--- squid/files/replace.py
+++ squid/files/replace.py
@@ -9,26 +9,43 @@ import re
 # read call parameters
 FD = sys.argv[1]
 KEYWORD = sys.argv[2]
-ENV = {}
-
-# open the config for reading
+REPLACEMENTDICT = {}
+# open the config for reading, we replace in memory
 try:
     with open(FD, 'r') as CONFIGFILE:
         CONFIGDATA = CONFIGFILE.read()
-        #CONFIGDATA = re.sub(r'(?m)^ *#.*\n?', '', CONFIGFILE)
+        CONFIGFILE.close()
+# strip comments because they are not needed in containers
+        CONFIGDATA = re.sub(r'#.*\n',
+                            r'',
+                            CONFIGDATA)
 except IOError:
     exit(1)
 
 # read the environment variables that match
-for i in sorted(os.environ):
-    if KEYWORD in i:
-        y = os.environ[i]
-        ENV[i] = y
+for ENV_VAR in sorted(os.environ):
+    if KEYWORD in ENV_VAR:
+        KEY = re.sub(r'=.*\n',
+                     r'',
+                     ENV_VAR)
+        VALUE = os.environ[ENV_VAR]
+        REPLACEMENTDICT[KEY] = VALUE
 
-# find and replace in the config file
-for i in ENV:
-    if i in CONFIGDATA:
-        CONFIGDATA.replace(i, ENV[i])
 
-print(CONFIGDATA)
+# find and substitute env variables based on keyword prefix in the config file
+for KEY, VALUE in REPLACEMENTDICT.items():
+#    print(KEY, VALUE)
+    if KEY in CONFIGDATA:
+        CONFIGDATA = re.sub(KEY,
+                            VALUE,
+                            CONFIGDATA)
+
+# open the config again for writing
+try:
+    with open(FD, 'w') as CONFIGFILE:
+        CONFIGFILE.write(CONFIGDATA)
+except IOError:
+    exit(1)
+
+# close politely
 CONFIGFILE.close()
