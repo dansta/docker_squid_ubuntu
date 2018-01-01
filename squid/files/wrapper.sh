14536c60621ab8c66f14650d97b1c1c9dfe2a436
dansta <danne@kvantmekanik.se>
2018-01-01 20:53:36 +0100

    Adding a few more networks again

--- squid/files/wrapper.sh
+++ squid/files/wrapper.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -x
+
 if ( /usr/sbin/squid -k parse ); then
   echo Parsing complete
   if ( /usr/sbin/squid -z ); then
     echo Created cache_dir
     sleep 60
-    if ( /usr/sbin/squid -sNd1F ); then
+    if ( /usr/sbin/squid -sNd2F ); then
        echo Exited squid
     fi
   fi
