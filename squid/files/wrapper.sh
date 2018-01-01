3acb9f15d680ec08c8fb2ef7d7c52c04ed0db3d1
dansta <danne@kvantmekanik.se>
2018-01-01 17:56:29 +0100

    Lets force it to sleep for 60 seconds instead
    It seems like some process is exiting and advancing the script
    before we know if the process is successful or not

--- squid/files/wrapper.sh
+++ squid/files/wrapper.sh
@@ -4,11 +4,9 @@ if ( /usr/sbin/squid -k parse ); then
   echo Parsing complete
   if ( /usr/sbin/squid -z ); then
     echo Created cache_dir
-      if ( /usr/sbin/squid -z ); then
-      echo Created cache_dir
-       if ( /usr/sbin/squid -sNd1F ); then
-         echo Exited squid
-       fi
+    sleep 60
+    if ( /usr/sbin/squid -sNd1F ); then
+       echo Exited squid
     fi
   fi
 fi
