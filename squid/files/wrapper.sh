e87639b921daba26d10983333a0ee34f1c2d95bf
dansta <danne@kvantmekanik.se>
2018-01-01 17:50:02 +0100

    Testing this

--- squid/files/wrapper.sh
+++ squid/files/wrapper.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-set -x
-
 if ( /usr/sbin/squid -k parse ); then
   echo Parsing complete
   if ( /usr/sbin/squid -z ); then
     echo Created cache_dir
-    echo Sleeping 5
-    sleep 5
-    if ( /usr/sbin/squid -sNd1F ); then
-      echo Exited squid
+      if ( /usr/sbin/squid -z ); then
+      echo Created cache_dir
+       if ( /usr/sbin/squid -sNd1F ); then
+         echo Exited squid
+       fi
     fi
   fi
 fi
