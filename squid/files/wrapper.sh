c35ac0662338ee544e7beb60024a7f18a5faafbb
dansta <danne@kvantmekanik.se>
2018-01-02 11:40:35 +0100

    Testing a few more things

--- squid/files/wrapper.sh
+++ squid/files/wrapper.sh
@@ -7,7 +7,7 @@ if ( /usr/sbin/squid -k parse ); then
   if ( /usr/sbin/squid -z ); then
     echo Created cache_dir
     sleep 60
-    if ( /usr/sbin/squid -sNd2F ); then
+    if ( /usr/sbin/squid -sNd2F --enable-cache-digests --enable-htcp --enable-icmp --enable-async-io ); then
        echo Exited squid
 # for debugging purposes only
        cat /var/log/squid/*
