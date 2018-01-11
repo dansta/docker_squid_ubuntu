2c23718f1e6e9b788ae9e4e2ee5da823205291f7
Daniel Ståhl <danne@kvantmekanik.se>
2018-01-11 13:25:35 +0100

    Running squid with -F should be explained as some people dont want to wait for the cache rebuild

--- squid/files/wrapper.sh
+++ squid/files/wrapper.sh
@@ -7,6 +7,7 @@ if ( /usr/sbin/squid -k parse ); then
   if ( /usr/sbin/squid -z ); then
     echo Created cache_dir
     sleep 60
+# Remove -F to make it start faster but without rebuilding cache first
     if ( /usr/sbin/squid -sNF ); then
        echo Exited squid
     fi
